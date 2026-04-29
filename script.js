bc9ad7acd9b4d9ba2055e2d88f3f51a577baeb63
Yash Chohtel <yashchohtel@gmail.com>
2026-04-29 23:30:58 +0530

    learning gsap scrolltrigger started

---
+++ script.js
@@ -0,0 +1,15 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".page1 .box", {
+    scale: 0,
+    delay: 1,
+    duration: 2,
+    rotate: 360,
+})
+
+gsap.from(".page2 .box", {
+    scale: 0,
+    delay: 1,
+    duration: 2,
+    rotate: 360,
+})
