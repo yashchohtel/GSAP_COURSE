214d4fd4e81735cb9f58b54f49a64af5c7243e9c
Yash Chohtel <yashchohtel@gmail.com>
2026-08-30 22:53:12 +0530

    course started again problem one solved

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './style.css'
+import { gsap } from "gsap";
+
+/* ============================================================= */
+
+/* PRACTICE QUESTION 1 ↓  */
+
+gsap.to(".moveFirst", {
+  x: 500,
+  duration: 2,
+  rotation: 360,
+  ease: "power2.out"
+});
+
+/* PRACTICE QUESTION 1 ↑  */
+
+/* ============================================================= */ 
